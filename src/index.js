d260d82ef36f9f4286691c64979d6677ddaab624
onmedicines <semwalanurag26@gmail.com>
2024-09-06 11:32:21 +0530

    Redesign the calculator functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+class Calculator {
+  constructor(prevOperandTextContent, currentOperandTextContent) {
+    this._prevOperandTextContent = prevOperandTextContent;
+    this._currentOparandTextContent = currentOperandTextContent;
+  }
+
+  clear() {
+    this.prevOperand = "";
+    this.currentOperand = "";
+    this.operation = undefined;
+  }
+}
+
+const calculator = new Calculator();
